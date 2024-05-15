435d5d24609c2d783a90740d5982986bf67335aa
baayaouiimane <167249908+baayaouiimane@users.noreply.github.com>
2024-05-15 20:23:05 +0100

    Delete hopitalll directory

--- hopitalll/src/main/java/ma/fsm/hopitalll/security/repo/AppRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.fsm.hopitalll.security.repo;
-
-import ma.fsm.hopitalll.security.entities.AppRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppRoleRepository extends JpaRepository<AppRole,String>
-{
-
-}
--- hopitalll/src/main/java/ma/fsm/hopitalll/security/repo/AppUserRepository.java
+++
@@ -1,9 +0,0 @@
-package ma.fsm.hopitalll.security.repo;
-
-import ma.fsm.hopitalll.security.entities.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser,String>
-{
-    AppUser findByUsername(String username);
-}
